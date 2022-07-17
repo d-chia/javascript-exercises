249b808a64a73018cfb13336bb310ac08626d93d
David C <d.chia@proton.me>
2022-07-18 01:19:21 +0200

    Add functions to convert between F and C temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(inputFahrenheit) {
+  let tempCelsius = (inputFahrenheit - 32) / 1.8;
+  return (Math.round(tempCelsius * 10))/10;
 };
 
-const ctof = function() {
-
+const ctof = function(inputCelsius) {
+  let tempFahrenheit = (inputCelsius * 1.8) + 32;
+  return (Math.round(tempFahrenheit*10))/10;
 };
 
 // Do not edit below this line
