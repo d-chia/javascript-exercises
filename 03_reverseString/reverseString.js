38ad134f91719447e5f21c6edecd7e765410c6db
David C <d.chia@proton.me>
2022-07-17 19:35:12 +0200

    Add function to reverse input string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringArray = Array.from(string);
+    stringArray.reverse();
+    return stringArray.join("");
 };
 
 // Do not edit below this line
