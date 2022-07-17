f452e005993988e2d4f160213915ff54a2800854
David C <d.chia@proton.me>
2022-07-18 01:03:36 +0200

    Add function to determine leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+//take year as input
+//check if divisible by 4
+//if yes, check if not divisible by 100
+//if yes, check if divisible by 400
+//if all conditions are met: return leap year
 
+const leapYears = function(year) {
+    if (year % 400 == 0) {
+        return true;
+    } else if ((year %  4) == 0 && (year % 100) != 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
