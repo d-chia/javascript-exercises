a29634f423ddb6ed82790bab678bead7c6b7761c
David C <d.chia@proton.me>
2022-07-18 00:06:16 +0200

    Add function to sum all integers between two numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
+// take input of two integers
+// add first integer to sum
+// add first integer+1 to sum
+// continue until second integer
+// return total sum
 
+let totalSum = null;
+
+const sumAll = function(firstInt, secondInt) {
+    for (let i = firstInt; i <= secondInt; i++) {
+        totalSum += i;
+    }
+    return totalSum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
