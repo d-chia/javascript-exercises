c8762433c36963579ef2720df94a53ee0ba2f7d9
David C <d.chia@proton.me>
2022-07-17 20:30:47 +0200

    Add rest parameter to turn arguments into array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,14 @@
-const removeFromArray = function(inputArray, removeElement) {
-    for (let arrayElement of inputArray) {
-        if (arrayElement == removeElement) {
-            inputArray.splice(inputArray.indexOf(arrayElement), 1);
-        } else {continue};
+// rest argument in function call (...removeArray) turns multiple arguments into array
+// loop through all elements from removeArray and check input for identical elements
+// remove if identical via splice and indexOf the element
+
+const removeFromArray = function(inputArray, ...removeArray) {
+    for (let removeElement of removeArray) {
+        for (let arrayElement of inputArray) {
+            if (arrayElement == removeElement) {
+                inputArray.splice(inputArray.indexOf(arrayElement), 1);
+            } else {continue};
+        }
     }
     return inputArray;
 };
