04ba4296d790f686b5c33150a33a2e323172390f
David C <d.chia@proton.me>
2022-07-27 01:36:05 +0200

    Add solution functions for calculations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,36 @@
-const add = function() {
+const add = function(...input) {
+  const sum = input.reduce((a, b) => +a + +b, 0);
+  return sum; 
+};
+
+const subtract = function(...input) {
+  const remainder = input.reduce((a, b) => +a - +b);
+	return remainder;
+};
+
+const sum = function([...input]) {
+  const sum = input.reduce((a, b) => +a + +b, 0);
+  return sum; 
+};
+
+const multiply = function([...input]) {
+  const product = input.reduce((a, b) => +a * +b);
+  return product;
+};
+
+const power = function(...input) {
+  const result = input.reduce((a, b) => a**b);
+  return result;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const factorial = function(n) {
+  let fact = 1;
+  for (i = n; i >= 1; i--) {
+    fact *= i;
+  }
+  return fact;
+}
 
 // Do not edit below this line
 module.exports = {
