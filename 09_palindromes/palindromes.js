d83dc1a75b5133ecff3e2b9025a4214ae086e76a
David C <d.chia@proton.me>
2022-07-28 01:01:14 +0200

    Refactor and cleanup

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,38 +1,38 @@
 const palindromes = function (string) {
 
-//turn input into an array
-    const inputArray = [...string];
+//turn input into an array and of lower case letters
+    const inputArray = [...string.toLowerCase()];
 
-//turn all elements into all lower case
-
-    const lowerCaseArray = inputArray.map(character => {
-        return character.toLowerCase();
-        }
-    )
-
-//remove or filter non-alphanumerical from array
+//filter array for alphanumerical characters
 
     const forwardArray = lowerCaseArray.filter(character => character >= "a" && character <= "z");
 
 //create reverse array
 
     const reverseArray = [...forwardArray].reverse();
- 
-//check if array is identical in both directions by looping and comparing each element
 
-
-//comparing arrays for equality seems tricky --> join to get string and compare instead
-    // for (let i = 0; i <= forwardArray.length; i++) {
-    //     if (forwardArray[i] != reverseArray[i]) {
-    //         return false;
-    //     } else {
-    //        continue;
-    //     }
-    // }
+//join() arrays to get string and compare for equality. return boolean
 
     return forwardArray.join() == reverseArray.join();
 
 };
+
+
+
+
+//note: comparing arrays for equality seems tricky. not working:
+    //return forwardArray == reverseArray;
+
+//looping over each element also not working
+    // for (let i = 0; i <= forwardArray.length; i++) {
+    //     if (forwardArray[i] != reverseArray[i]) {
+    //         return false;
+    //     };
+    // }
+
+//--> join() to get string and compare instead.
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
