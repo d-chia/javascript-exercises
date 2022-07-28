7c5e2a5289a09dbb2140c8b5de2d51b3c06d334e
David C <d.chia@proton.me>
2022-07-28 19:50:26 +0200

    Add solution with forEach loop

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,4 +1,14 @@
-const getTheTitles = function() {
+const getTheTitles = function(searchArray) {
+
+    //declare empty array for titles
+    const titles = [];
+
+    //loop over all objects in the array and push titles
+    searchArray.forEach(element => titles.push(element.title));
+
+    //return titles array
+
+    return titles;
 
 };
 
