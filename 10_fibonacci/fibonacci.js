b69ae8e16d266131ad492f8abe1fccded80a0a6f
David C <d.chia@proton.me>
2022-07-28 19:31:11 +0200

    Add initial solution for function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(inputNum) {
+    //create array
+    const sequence = [];
+    
+    //fill array with fibonacci numbers until input
+    for (let i = 0; i < inputNum; i++) {
+        if (i <= 1)  {
+            let fibonacciNum = 1;
+            sequence.push(fibonacciNum);
+        } else if (i >= 2) {
+            let fibonacciNum = (sequence[(i - 1)] + sequence[(i - 2)]);
+            sequence.push(fibonacciNum);
+        }
+    }
+    //return input-nth number with 1 being first element (that's why -1)
+    return sequence[(inputNum - 1)];
 };
 
 // Do not edit below this line
